d06bade00b63b198ea12cefe483a6184b28a7d34
lucasdantas2014 <lucas.dantasifpb@gmail.com>
2018-09-12 09:25:58 -0300

    Get e Set

--- src/Disciplina.java
+++ src/Disciplina.java
@@ -1,5 +1,26 @@
+
 public class Disciplina {
 	private String Acronimo;
 	private String NomeProf;
 	private String NomeDisc;
+	
+	public String getAcronimo() {
+		return Acronimo;
+	}
+	public void setAcronimo(String acronimo) {
+		Acronimo = acronimo;
+	}
+	public String getNomeProf() {
+		return NomeProf;
+	}
+	public void setNomeProf(String nomeProf) {
+		NomeProf = nomeProf;
+	}
+	public String getNomeDisc() {
+		return NomeDisc;
+	}
+	public void setNomeDisc(String nomeDisc) {
+		NomeDisc = nomeDisc;
+	}
+	
 }
