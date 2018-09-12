46fca67f2a879c843c494cc775ef9fb292df8174
Lucas Ferreira <lucasnatacao@gmail.com>
2018-09-12 09:29:59 -0300

    Update Usuario.java

--- src/Usuario.java
+++ src/Usuario.java
@@ -5,8 +5,17 @@ public class Usuario {
 	private String Curso;
 	private String[] tags = new String[50];
 	
-	public void CadastrarAtiv(){
+	public void CadastrarAtiv(String[] args){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Digite o nome da atividade: ");
+		String Nome = sc.next();
 		
+		System.out.println(Nome);
+		
+		System.out.println("Digite alguma das tags pré-programadas: Projeto; Prova; Monitoria; Miniteste; Seminário: ");
+		String Tags = sc.next();
+		
+		System.out.println(Tags);
 	}
 	public void CadastrarDisc(){
 		
