88e68de26b33f8537a78293cd7730906442141bd
lucasdantas2014 <lucas.dantasifpb@gmail.com>
2018-09-12 09:01:24 -0300

    Add files via upload

---
+++ src/Atividade.java
@@ -0,0 +1,56 @@
+
+public class Atividade {
+	private String nome;
+	private String deadline;
+	private String[] tags = new String[50];
+	private boolean andamento;
+	private Disciplina disciplina;
+	
+	public Atividade(String nome,String deadline, String[] tags, Disciplina disciplina){
+		this.nome = nome;
+		this.deadline = deadline;
+		this.tags = tags;
+		this.disciplina = disciplina;
+		
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDeadline() {
+		return deadline;
+	}
+
+	public void setDeadline(String deadline) {
+		this.deadline = deadline;
+	}
+
+	public String[] getTags() {
+		return tags;
+	}
+
+	public void setTags(String[] tags) {
+		this.tags = tags;
+	}
+
+	public boolean isAndamento() {
+		return andamento;
+	}
+
+	public void setAndamento(boolean andamento) {
+		this.andamento = andamento;
+	}
+
+	public Disciplina getDisciplina() {
+		return disciplina;
+	}
+
+	public void setDisciplina(Disciplina disciplina) {
+		this.disciplina = disciplina;
+	}
+}
