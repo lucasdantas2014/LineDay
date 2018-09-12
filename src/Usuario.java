d095f91dea78f38b25336abcf0b54a1727ccdc68
Lucas Ferreira <lucasnatacao@gmail.com>
2018-09-12 09:10:25 -0300

    Add files via upload

---
+++ src/Usuario.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Usuario {
+	private String Nome;
+	private String Curso;
+	private String[] tags = new String[50];
+	
+	public void CadastrarAtiv(){
+		
+	}
+	public void CadastrarDisc(){
+		
+	}
+	public void ExcluirAtiv(){
+		
+	}
+	public void EditarPerfil(){
+		
+	}
+}
