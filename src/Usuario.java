9aab0018d3d0ab76df4a5f305b500a7d6b06af9d
lucasdantas2014 <lucas.dantasifpb@gmail.com>
2018-09-17 14:33:34 -0300

    Update Usuario.java

--- src/Usuario.java
+++ src/Usuario.java
@@ -1,126 +1,75 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.awt.datatransfer.SystemFlavorMap;
 import java.util.Scanner;
+import java.util.concurrent.SynchronousQueue;
 
-public class Usuario {
-	private String Nome;
-	private String Curso;
-	private String[] tags = new String[50];
-	private Atividade[] atividades = new Atividade[1000];
-	private Disciplina[] disciplinas = new Disciplina[100];
-	
-	
-	//Cadastrar Atividade
-	public void CadastrarAtiv(Atividade atividade){
+
+public class Main {
+	public static void menu(){
 		
-			for(int n = 0; n < this.disciplinas.length; n++){
-				if(this.atividades[n] == null){
-					this.atividades[n] = atividade;
-					break;
-				}
+		System.out.println("------Menu------");
+		System.out.println("1 - Editar Usuario ");
+		System.out.println("2 - Cadastrar Atividades:");
+		System.out.println("3 - Cadastrar Discplina");
+		System.out.println("4 - Exibir Disciplina");
+		System.out.println("5 - Exibir Atividades");
+		System.out.println("6 - Buscar Disciplina por Acronimo");
+
+		
+
+	}
+	
+	public static void main(String[] args) {
+		Scanner inputStr = new Scanner(System.in); // Scaner String
+		Scanner input = new Scanner(System.in); // Scaner Int e outros
+
+		Fachada fachada = new Fachada();
+		Usuario user = new Usuario(); // Usuario
+		int opcao; // 
+		menu();
+		opcao = input.nextInt();
+		
+		while (opcao != 0){
+			
+			
+//			Editar opcoes do usuario 
+			if(opcao == 1){		
+				
+				fachada.editarUsuario(user);;
+				
 			}
 			
-		}
-	
-	//Cadastrar Discilpina
-	public void CadastrarDisc(Disciplina disc){
-		int n;
-		for(n = 0; n < this.disciplinas.length; n++){
-			if(this.disciplinas[n] == null){
-				this.disciplinas[n] = disc;
-				break;
+			//Adicionando atividades 
+			else if(opcao == 2){
+				fachada.cadastrarAtividade(user);
 			}
-		}
-		
-	}
-	
-	//Excluir Atividade
-	public void ExcluirAtiv(){
-		
-	}
-	
-	//Exibir todas as discilplinas percorrendo a lista
-	public void exibirDisciplinas(){
-		
-		for(int i=0;this.disciplinas[i] != null; i++){
-		    System.out.println(this.disciplinas[i]);
-		}
-	}
-	
-	//Busca disicplinas pelo acronimo
-	public Disciplina buscarDiscPorAcronimo(String acronimo){
-		
-		for(int i=0; this.disciplinas[i] != null; i++){
-		    if(acronimo.equals(this.disciplinas[i].getAcronimo())){
-		    	
-		    	return this.disciplinas[i]; 
-		    	
-		    }
-		    	
-		    
-		}
-		//Caso nao encontre
-		System.out.println("NAO FOI ENCOPNTRADO");
-		return null;
-		
-	}
-	
-	//Exibir atividade percorrendo a lista
-	public void exibirAtividades(){
 			
-			for(int i=0;this.atividades[i] != null; i++){
-			    System.out.println(this.atividades[i]);
+			//Cadastrando disciplina
+			else if(opcao == 3){				
+				fachada.cadastrarDisicplina(user);
+				
 			}
-		}
-	
-	
-	
-	@Override
-	public String toString() {
-		return "Usuario [Nome=" + Nome + ", Curso=" + Curso + ", tags=" + Arrays.toString(tags) + ", atividades="
-				+ Arrays.toString(atividades) + ", disciplinas=" + Arrays.toString(disciplinas) + "]";
-	}
-	public void EditarPerfil(){
-		
-	}
-	
-	public String getNome() {
-		return Nome;
-	}
-	public void setNome(String nome) {
-		Nome = nome;
-	}
-	public String getCurso() {
-		return Curso;
-	}
-	public void setCurso(String curso) {
-		Curso = curso;
-	}
-	public String[] getTags() {
-		return tags;
-	}
-	public void setTags(String[] tags) {
-		this.tags = tags;
-	}
-	public Atividade[] getAtividades() {
-		return atividades;
-	}
-	public void setAtividades(Atividade[] atividades) {
-		this.atividades = atividades;
-	}
-	
-	public void setDisciplinas(Disciplina[] disciplinas) {
-		this.disciplinas = disciplinas;
-	}
-	public void adicionarTag(String novaTag) {
-		
-		for(int n = 0; n < this.tags.length; n++){
-			if(this.tags[n] == null){
-				this.tags[n] = novaTag;
-				break;
+			
+			else if(opcao == 4){
+				fachada.exibirDisciplinas(user);
 			}
+			
+			else if(opcao == 5){
+				fachada.exibirAtividades(user);
+			}
+			
+			else if(opcao == 6){
+				fachada.buscarPorAcronimo(user);
+				
+			}
+			menu();
+			opcao = input.nextInt();
+
 		}
+		
+		
+
 	}
 	
+	
+
 }
