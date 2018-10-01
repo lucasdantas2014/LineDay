c2c0806cefd874c0574e8f7857e31d3e02cf3c0c
lucasdantas2014 <lucas.dantasifpb@gmail.com>
2018-10-01 15:44:39 -0300

    Update AtividadeDAO.java

--- src/database/AtividadeDAO.java
+++ src/database/AtividadeDAO.java
@@ -1,5 +1,31 @@
 package database;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
 
-public class AtividadeDAO {
+import model.Atividade;
 
+public class AtividadeDAO extends DAO{
+	public void insert(Atividade atividade){
+		String sql = "insert into atividade(nome,deadline,disciplina)"
+					+"values(?,?,?);";
+				
+		try (Connection con = connect();
+	              PreparedStatement pstmt = con.prepareStatement(sql)) {
+
+	          pstmt.setString(1, atividade.getNome());
+	          pstmt.setDate(2, (Date) atividade.getDeadline());
+	          pstmt.setInt(3, atividade.getDisciplina());
+
+
+	      } catch (SQLException ex) {
+	          System.out.println(ex.getMessage());
+	      }
+		
+		
+	}
 }
